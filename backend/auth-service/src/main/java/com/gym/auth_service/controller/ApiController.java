b546bfea0567a4109b6ff2a56049fb15fd3d00db
Asfar-07 <asfarbismi@gmail.com>
2026-01-23 22:44:04 +0530

    Update 2 auth server ( create jwt )

--- backend/auth-service/src/main/java/com/gym/auth_service/controller/ApiController.java
+++ backend/auth-service/src/main/java/com/gym/auth_service/controller/ApiController.java
@@ -1,17 +1,57 @@
 package com.gym.auth_service.controller;
 
+import com.gym.auth_service.model.UserDataModel;
+import com.gym.auth_service.service.UserService;
+import com.gym.auth_service.utils.JWTManage;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.swing.*;
+
+@RestController
+@RequestMapping("/auth")
 public class ApiController {
-//    @Autowired
+    @Autowired
+    JWTManage jwtManage;
+    UserService service;
+
+    @GetMapping(value = "/")
+    public  String Home(){
+
+        return "Hello";
+    }
 
     @PostMapping(value = "/login")
     public  void Login(){
 
     }
-
+    @PostMapping(value = "/signup")
+    public ResponseEntity<String> SignUp(@RequestBody UserDataModel userData, HttpServletResponse response){
+        String Token= jwtManage.generateToken(userData.getEmail());
+        System.out.println(userData.getEmail());
+        ResponseCookie cookie= ResponseCookie.from("JWT",Token).httpOnly(true)
+                .secure(false)
+                .path("/")
+                .sameSite("Lax")
+                .maxAge(15 * 60)
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE,cookie.toString());
+//        service.signupService(userData);
+        return ResponseEntity.ok("202");
+    }
+    @PostMapping(value = "/logout")
+    public ResponseEntity<String> Logout(HttpServletResponse response){
+        ResponseCookie cookie= ResponseCookie.from("JWT",null).httpOnly(true)
+                .secure(false)
+                .path("/")
+                .sameSite("Lax")
+                .maxAge(0)
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE,cookie.toString());
+        return ResponseEntity.ok("202");
+    }
 }
---
+++ backend/auth-service/src/main/java/com/gym/auth_service/utils/JWTManage.java
@@ -0,0 +1,24 @@
+package com.gym.auth_service.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTManage {
+    private final String SECRET = "my-super-secret-key-which-is-at-least-32-characters";
+    public String generateToken(String email){
+        long expireTime = 1000 * 60 * 15;
+        String token = Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+ expireTime))
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+        return token;
+    }
+
+}
