8487be5af1b9a0b577abe726415596e4cd6d5579
Asfar-07 <asfarbismi@gmail.com>
2026-01-23 22:05:52 +0530

    Update 3 gateway (set http cookie)

--- backend/api-gateway/src/main/java/com/gym/api_gateway/filter/JwtAuthFilter.java
+++ backend/api-gateway/src/main/java/com/gym/api_gateway/filter/JwtAuthFilter.java
@@ -3,7 +3,9 @@ package com.gym.api_gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -23,6 +25,14 @@ public class JwtAuthFilter implements GlobalFilter, Ordered {
 
         if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
             return chain.filter(exchange);
+        }
+        HttpCookie cookie = exchange.getRequest()
+                .getCookies()
+                .getFirst("JWT");
+
+        if (cookie == null) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
         }
         return chain.filter(exchange);
     }
