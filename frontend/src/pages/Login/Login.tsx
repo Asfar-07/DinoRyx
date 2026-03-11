ecae9b9dd54a91e3a8da9e146f5ea356a306e9e5
Asfar-07 <asfarbismi@gmail.com>
2026-03-11 19:55:40 +0530

    make strong validation on login page (hook from)

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -19,16 +19,25 @@ export default function Login() {
   }
   const [isSignup, setIsSignup] = useState(false);
   const [isLoading,setIsLoading] = useState(false);
-  const loginForm=useForm<userForm>();
-  const {register,handleSubmit}=loginForm;
+  const loginForm=useForm<userForm>({defaultValues:{username:"",email:"",password:""}});
+  const {register,handleSubmit,formState,reset}=loginForm;
+  const {errors}=formState;
 
   let navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const resetDefault=()=>{
+    reset({
+      username:"",
+      email:"",
+      password:""
+    })
+  }
   const handleGoogleAuth = async (credentialResponse:any) => {
     const googleToken = credentialResponse.credential;
     authHandle.googleService(googleToken);
   };
+
   function authenticationSubmit(userData:userForm) {
     if(isLoading) return;
     setIsLoading(true);
@@ -38,6 +47,7 @@ export default function Login() {
         .then((data) => {
             dispatch(addUser(data))
             dispatch(updateAuth(true));
+            resetDefault()
             setIsLoading(false);
             navigate("/");
         })
@@ -51,6 +61,7 @@ export default function Login() {
         .then((data) => {
            dispatch(addUser(data))
             dispatch(updateAuth(true));
+            resetDefault()
             setIsLoading(false);
             navigate("/");
         })
@@ -58,6 +69,18 @@ export default function Login() {
           setIsLoading(false);
           console.log(err);
         });
+    }
+  }
+  function errorValidation(){
+    if(errors.email?.message && errors.password?.message){
+      return "Please enter valid email, password";
+    }if (errors.email?.message) {
+      return errors.email?.message;
+    }if (errors.password?.message) {
+      if(isSignup) return errors.password?.message;
+      return "Please enter valid password";
+    }else{
+      if(isSignup) return (errors.username?.message);
     }
   }
 
@@ -86,6 +109,7 @@ export default function Login() {
             <button className="bg-(--symbol-color) hover:opacity-90 transition-all text(--primary-bg-color) font-bold py-2.5 px-12 rounded-lg text-lg cursor-pointer"
             onClick={()=>{
               isSignup ? setIsSignup(false):setIsSignup(true);
+              resetDefault()
             }}>
               {isSignup?"Log In" : "Sign In"}
             </button>
@@ -99,15 +123,22 @@ export default function Login() {
 
         {/* Right Side: Form Area */}
         <div className="login-right bg-(--secondary-bg-color)  z-2 md:w-[50%] p-5 md:p-10 flex flex-col justify-center">
-          <h2 className="text-[clamp(1rem,calc(3vw+1rem),2rem)] font-semibold text-(--primary-text-color) mb-8">{isSignup?"Sign In" : "Log In"}</h2>
-          
+         <header className="flex justify-between">
+           <h2 className="text-[clamp(1rem,calc(3vw+1rem),2rem)] font-semibold text-(--primary-text-color) mb-8">{isSignup ? "Sign In" : "Log In"}</h2>
+           <Link to="/" className="text-(--primary-text-color)">Home</Link>
+         </header>          
           <form className="space-y-4" onSubmit={handleSubmit(authenticationSubmit)} noValidate>
            {isSignup && <div>
              <label className="block text-(--secondary-text-color) text-sm mb-2 font-medium">Username</label>
              <input
                type="text"
                placeholder="Enter your username"
-               {...register("username")}
+               {...register("username", {
+                 required: "Please enter username", pattern: {
+                   value: /^[A-Za-z ]{3,20}$/,
+                   message: "Username must be 3–15 letters (spaces allowed, no numbers)"
+                 }
+               })}
                className="w-full bg-(--primary-bg-color) border border-white/10 rounded-lg p-3.5 text-(--primary-text-color) focus:outline-none focus:ring-1 focus:ring-(--symbol-color) transition-all placeholder:text-gray-600"
              />
            </div>}
@@ -117,7 +148,12 @@ export default function Login() {
               <input 
                 type="email" 
                 placeholder="Enter your email" 
-                {...register("email")}
+               {...register("email", {
+                 required: "Please enter email", pattern: {
+                   value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                   message: "Invalid email"
+                 }
+               })}
                 className="w-full bg-(--primary-bg-color) border border-white/10 rounded-lg p-3.5 text-(--primary-text-color) focus:outline-none focus:ring-1 focus:ring-(--symbol-color) transition-all placeholder:text-gray-600"
               />
             </div>
@@ -127,10 +163,17 @@ export default function Login() {
               <input 
                 type="password" 
                 placeholder="Enter your password" 
-                {...register("password")}
+               {...register("password", {
+                 required: "Please enter password", pattern: {
+                   value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/,
+                   message:
+                     "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one symbol"
+                 }
+               })}
                 className="w-full bg-(--primary-bg-color) border border-white/10 rounded-lg p-3.5 text-(--primary-text-color) focus:outline-none focus:ring-1 focus:ring-(--symbol-color) transition-all placeholder:text-gray-600"
               />
             </div>
+            <p className=" text-red-600 my-2">{errorValidation()}</p>
 
             <div className="text-right">
               <Link to="/forgot-password" className="text-(--symbol-color) text-sm font-medium hover:underline decoration-1 underline-offset-4">
@@ -152,7 +195,7 @@ export default function Login() {
 
           {/* Social Icons Section */}
          <div className="flex gap-4 mb-8">
-           <button className="flex-1 border border-white/10 rounded-lg py-1.5 hover:bg-white/5 transition-colors flex justify-center items-center">
+           <button className="flex-1 border border-white/10 rounded-lg  hover:bg-white/5 transition-colors flex justify-center items-center">
              <GoogleLogin
                onSuccess={handleGoogleAuth}
                onError={() => console.log("Login Failed")}
@@ -168,6 +211,7 @@ export default function Login() {
            <button className="text-(--primary-text-color) font-bold hover:underline decoration-(--symbol-color) underline-offset-4"
              onClick={() => {
                isSignup ? setIsSignup(false) : setIsSignup(true);
+               resetDefault()
              }}
            >{isSignup ? "Log In" : "Sign In"}</button>
          </p>
